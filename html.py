9676bb17adaa1d1f7f9d5effdcd242067abd8465
carbohydrate08 <e6a201052d@edu.teu.ac.jp>
2022-02-01 22:18:13 +0900

    [Update]pro2

--- html.py
+++ html.py
@@ -2,19 +2,22 @@ import requests
 
 
 def pro1():  # urlのhtmlファイルを取得する
-    url = "https://api.aoikujira.com/time/get.php"
-    result = requests.get(url)
+  url = "https://api.aoikujira.com/time/get.php"
+  result = requests.get(url)
 
-    print(result.text)
+  print(result.text)
 
 
 def pro2():  # urlから単一の画像を取り出す
-    url = "https://uta.pw/shodou/img/3/3.png"
-    res = requests.get(url)
-    if not res.ok:
-        print("失敗：", res.status_code)
-        quit()
+  url = "https://uta.pw/shodou/img/3/3.png"
+  res = requests.get(url)
+  if not res.ok:
+      print("失敗：", res.status_code)
+      quit()
 
+  with open("gyudon.png", "wb") as fp:
+    fp.write(res.content)
+  print("ok.")
 
 if __name__ == '__main__':
     pro1()
