0fd146c9568dd0bf4b8354c86af2710ef7188336
carbohydrate08 <e6a201052d@edu.teu.ac.jp>
2022-02-01 22:20:53 +0900

    [Add]pro4

--- html.py
+++ html.py
@@ -1,5 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
+import urllib, os, time
+
 
 
 
@@ -32,6 +34,34 @@ def pro3(): #htmlファイルからタグ内の文字を読み取る
   print(title)
   print(title.text)
 
+
+def try4():
+  save_dir = "./pydoc_tutorial"
+  top_page = "https://docs.python.org/ja/3/tutorial/index.html"
+  check_url = "https://docs.python.org/ja/3/tutorial/"
+  visited = {}
+
+  def get_page(url):
+    if check_url not in url: return
+    if url in visited: return
+    visited[url] = True
+    res = requests.get(url)
+    res.encoding = res.apparent_encoding
+    html = res.text
+    fname = save_dir + "/" + os.path.basename(url)
+    if not os.path.exists(save_dir):
+      os.mkdir(save_dir)
+    with open(fname, "wt", encoding="utf-8") as f:
+      f.write(html)
+      print("save:", fname)
+    time.sleep(1)
+    soup = BeautifulSoup(html, "html5lib")
+    for a in soup.find_all("a"):
+      a_url = urllib.parse.urljoin(url, a["href"])
+      a_url = urllib.parse.urldefrag(a_url)[0]
+      get_page(a_url)
+
+  get_page(top_page)
 
 if __name__ == '__main__':
     pro1()
