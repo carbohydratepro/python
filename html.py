2f822a81cd46dab93756fe1c42f822664e536b0e
carbohydrate08 <e6a201052d@edu.teu.ac.jp>
2022-02-01 22:22:26 +0900

    [Add]pro5

--- html.py
+++ html.py
@@ -1,67 +1,78 @@
 import requests
 from bs4 import BeautifulSoup
-import urllib, os, time
-
-
+import urllib
+import os
+import time
+from selenium import webdriver
 
 
 def pro1():  # urlのhtmlファイルを取得する
-  url = "https://api.aoikujira.com/time/get.php"
-  result = requests.get(url)
+    url = "https://api.aoikujira.com/time/get.php"
+    result = requests.get(url)
 
-  print(result.text)
+    print(result.text)
 
 
 def pro2():  # urlから単一の画像を取り出す
-  url = "https://uta.pw/shodou/img/3/3.png"
-  res = requests.get(url)
-  if not res.ok:
-      print("失敗：", res.status_code)
-      quit()
-
-  with open("gyudon.png", "wb") as fp:
-    fp.write(res.content)
-  print("ok.")
-
-
-def pro3(): #htmlファイルからタグ内の文字を読み取る
-  with open("html_chapter10.html", encoding="utf-8") as fp:
-    html_str = fp.read()
-
-  soup = BeautifulSoup(html_str, "html5lib")
-
-  title = soup.find("title")
-  print(title)
-  print(title.text)
-
-
-def try4():
-  save_dir = "./pydoc_tutorial"
-  top_page = "https://docs.python.org/ja/3/tutorial/index.html"
-  check_url = "https://docs.python.org/ja/3/tutorial/"
-  visited = {}
-
-  def get_page(url):
-    if check_url not in url: return
-    if url in visited: return
-    visited[url] = True
+    url = "https://uta.pw/shodou/img/3/3.png"
     res = requests.get(url)
-    res.encoding = res.apparent_encoding
-    html = res.text
-    fname = save_dir + "/" + os.path.basename(url)
-    if not os.path.exists(save_dir):
-      os.mkdir(save_dir)
-    with open(fname, "wt", encoding="utf-8") as f:
-      f.write(html)
-      print("save:", fname)
-    time.sleep(1)
-    soup = BeautifulSoup(html, "html5lib")
-    for a in soup.find_all("a"):
-      a_url = urllib.parse.urljoin(url, a["href"])
-      a_url = urllib.parse.urldefrag(a_url)[0]
-      get_page(a_url)
+    if not res.ok:
+        print("失敗：", res.status_code)
+        quit()
 
-  get_page(top_page)
+    with open("gyudon.png", "wb") as fp:
+        fp.write(res.content)
+    print("ok.")
+
+
+def pro3():  # htmlファイルからタグ内の文字を読み取る
+    with open("html_chapter10.html", encoding="utf-8") as fp:
+        html_str = fp.read()
+
+    soup = BeautifulSoup(html_str, "html5lib")
+
+    title = soup.find("title")
+    print(title)
+    print(title.text)
+
+
+def pro4():
+    save_dir = "./pydoc_tutorial"
+    top_page = "https://docs.python.org/ja/3/tutorial/index.html"
+    check_url = "https://docs.python.org/ja/3/tutorial/"
+    visited = {}
+
+    def get_page(url):
+        if check_url not in url:
+            return
+        if url in visited:
+            return
+        visited[url] = True
+        res = requests.get(url)
+        res.encoding = res.apparent_encoding
+        html = res.text
+        fname = save_dir + "/" + os.path.basename(url)
+        if not os.path.exists(save_dir):
+            os.mkdir(save_dir)
+        with open(fname, "wt", encoding="utf-8") as f:
+            f.write(html)
+            print("save:", fname)
+        time.sleep(1)
+        soup = BeautifulSoup(html, "html5lib")
+        for a in soup.find_all("a"):
+            a_url = urllib.parse.urljoin(url, a["href"])
+            a_url = urllib.parse.urldefrag(a_url)[0]
+            get_page(a_url)
+
+    get_page(top_page)
+
+
+def pro5():  # webbrowser
+    driver = webdriver.Chrome()
+    driver.get("https://python.org")
+    time.sleep(10)
+    driver.quit()
+
 
 if __name__ == '__main__':
     pro1()
